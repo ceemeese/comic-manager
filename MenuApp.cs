bd7c41af93762934bead9880a3b4f0a614032abb
ceemeese <cmsalvador92@gmail.com>
2025-01-18 12:46:51 +0100

    feature: Reestructuración de proyecto. Se crea utils y services. Se pasan los métodos a estáticos

--- MenuApp.cs
+++ MenuApp.cs
@@ -1,16 +1,14 @@
 namespace Models; 
 using Models;
+using Services;
 using System.Text.RegularExpressions;
 
 class MenuApp
 {
-    private List<Genre> genres;
-    private List<Comic> comics;
 
     public MenuApp()
     {
-        genres = new List<Genre>();
-        comics = new List<Comic>();
+        
     }
 
     public void ShowMenu()
@@ -20,12 +18,12 @@ class MenuApp
         
         do
         {
-            Console.WriteLine("\n--- Menú gestor de cómics ---");
-            Console.WriteLine("1. Añadir Género");
-            Console.WriteLine("2. Añadir Cómic");
-            Console.WriteLine("3. Ver todos los géneros");
-            Console.WriteLine("4. Ver todos los cómics");
-            Console.WriteLine("5. Registrar usuario");
+            Console.WriteLine("\n--- MENÚ PRINCIPAL ---");
+            Console.WriteLine("1. Añadir Genero");
+            Console.WriteLine("2. Añadir Comic");
+            Console.WriteLine("3. Listar Generos");
+            Console.WriteLine("4. Listar Comics");
+            Console.WriteLine("5. Añadir Usuario Usuarios");
             Console.WriteLine("6. Salir");
             Console.WriteLine("Selecciona una opción:");
 
@@ -38,23 +36,22 @@ class MenuApp
             switch (option)
             {
                 case 1:
-                    addGenre();
+                    GenreService.addGenre();
                     break;
                 case 2:
-                    addComic();
+                    ComicService.addComic();
                     break;
                 case 3:
-                    showAllGenres();
+                    GenreService.showAllGenres();
                     break;
                 case 4:
-                    showAllComics();
+                    ComicService.showAllComics();
                     break;
                 case 5:
-                    userRegister();
+                    UserService.userRegister();
                     break;
                 case 6:
                     Console.WriteLine("¡Hasta pronto!");
-                    Environment.Exit(0);
                     break;
                 default:
                     Console.WriteLine("La opción no es correcta");
@@ -63,324 +60,4 @@ class MenuApp
         }
         while(option != 6);
     }
-
-
-
-    public void addGenre()
-    {
-        try
-        {
-            Console.WriteLine("___NUEVO GÉNERO___");
-            Console.WriteLine("Nombre: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Description: ");
-            string description = Console.ReadLine();
-
-            int priority;
-            while (true)
-            {
-                Console.WriteLine("Prioridad: ");
-                if (int.TryParse(Console.ReadLine(), out priority) && priority > 0)
-                {
-                    break;
-                }
-                Console.WriteLine("Error: La prioridad debe ser un número positivo.");
-            }
-
-
-            string icon;
-            while (true)
-            {
-                Console.WriteLine("Icono: ");
-                icon = Console.ReadLine();
-
-                if (icon.StartsWith('#'))
-                {
-                    break; 
-                }
-                Console.WriteLine("Error: El icono debe empezar por '#'.");
-            }
-            
-
-            Genre genre = new Genre(name, description, priority, icon);
-            genre.ShowGenreInformation();
-
-            genres.Add(genre);
-
-            Console.WriteLine("Género añadido correctamente.");
-
-        }
-        catch (InvalidGenreException ex) 
-        {
-            var messageError = "InvalidGenreException:" + ex.Message;
-            Console.WriteLine(messageError);
-        }
-        catch(Exception ex)
-        {
-            var messageError = "ExceptionError:" + ex.Message;
-        }
-    }
-
-
-
-    public void addComic() 
-    {
-        try
-        {
-            //Control de géneros antes de preguntar. Si no hay géneros, no se pueden añadir cómics
-            if (genres.Count == 0)
-            {
-                Console.WriteLine("No hay géneros disponibles. Añade algunos géneros antes de seleccionar.");
-                return;
-            }
-
-
-            Console.WriteLine("___NUEVO CÓMIC___");
-            Console.WriteLine("Nombre: ");
-            string name = Console.ReadLine();
-
-
-            Console.WriteLine("Autor: ");
-            string author
-            = Console.ReadLine();
-
-
-            Console.WriteLine("Editorial: ");
-            string publisher = Console.ReadLine();
-
-
-            //Validación año
-            int yearPublished;
-            int currentYear = DateTime.Now.Year;
-            while (true)
-            {
-                Console.WriteLine("Año de Publicación: ");
-                if (int.TryParse(Console.ReadLine(), out yearPublished) && yearPublished >= 1896 && yearPublished <= currentYear)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine($"Error: El año debe ser entre 1896 y {currentYear}.");
-                }
-            }
-
-
-            //Validación precio
-            decimal price;
-            while (true)
-            {
-                Console.WriteLine("Precio: ");
-                if (decimal.TryParse(Console.ReadLine(), out price) && price > 0)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Error: El precio debe ser un número positivo.");
-                }
-            }
-
-
-            Console.WriteLine("Leído? (si/no): ");
-            string answer = Console.ReadLine();
-            bool isRead = answer.ToLower() == "yes" ? true : false;
-
-
-            Console.WriteLine("Es para adultos? (si/no): ");
-            answer = Console.ReadLine();
-            bool isForAdults = answer.ToLower() == "yes" ? true : false;
-
-
-            List<Genre> selectedGenres = new List<Genre>();
-            while(true)
-            {
-                Console.WriteLine("Géneros disponibles: ");
-                for (int i = 0; i < genres.Count; i++)
-                {
-                    Console.WriteLine($"{i + 1}. {genres[i].Name}");
-                }
-
-                Console.WriteLine("Introduce los números de los géneros separados por comas (ejemplo: 1,3,5):");
-                answer = Console.ReadLine();
-
-
-                if (string.IsNullOrWhiteSpace(answer))
-                {
-                    Console.WriteLine("Error: No has seleccionado ninguna categoría. Intentelo de nuevo.");
-                    continue;
-                }
-
-                string[] genreIndexArray = answer.Split(',');
-                selectedGenres.Clear();
-
-                bool isValid = true;
-
-                foreach (string index in genreIndexArray)
-                {
-                    if (int.TryParse(index.Trim(), out int genreIndex) && genreIndex > 0 && genreIndex <= genres.Count)
-                    {
-                        selectedGenres.Add(genres[genreIndex - 1]);
-                    }
-                    else
-                    {
-                        Console.WriteLine($"Error: La opción '{index}' marcada no es correcta. Intentelo de nuevo.");
-                        isValid = false;
-                        break;
-                    }
-                }
-
-                if (isValid && selectedGenres.Count > 0)
-                {
-                    break;
-                }
-            }
-
-
-            Console.WriteLine("Tipos de cómics:");
-            foreach (var comicType in Enum.GetValues(typeof(Comic.ComicType)))
-            {
-                Console.WriteLine($"{(int)comicType}. {comicType}");
-            }
-
-            Comic.ComicType selectedType;
-            while (true)
-            {
-                Console.WriteLine("Selecciona el tipo de cómic (número):");
-                if (int.TryParse(Console.ReadLine(), out int comicTypeSelection) && comicTypeSelection > 0 && comicTypeSelection <= Enum.GetValues(typeof(Comic.ComicType)).Length)
-                {
-                    selectedType = (Comic.ComicType)comicTypeSelection; // Casting del integer al enum
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Error: Tipo de cómic no válido.");
-                }
-            }
-            
-            Comic comic= new Comic(name, author, publisher, yearPublished, price, isRead, isForAdults, selectedGenres, selectedType);
-            comic.ShowComicInformation();
-
-            foreach (var genre in selectedGenres)
-            {
-                genre.Comics.Add(comic);
-            }
-
-            comics.Add(comic);
-                
-        }
-        catch (InvalidComicException ex) 
-        {
-            var messageError = "InvalidComicException:" + ex.Message;
-            Console.WriteLine(messageError);
-        }
-        catch(Exception ex)
-        {
-            var messageError = "ExceptionError:" + ex.Message;
-        }
-    }      
-
-
-
-
-
-    public void showAllGenres()
-    {
-        Console.WriteLine("\nListado de Géneros:");
-        foreach (var genre in genres)
-        {
-            genre.ShowGenreInformation();
-        }
-    }
-
-    public void showAllComics()
-    {
-        Console.WriteLine("\nListado de Cómics:");
-        foreach (var comic in comics)
-        {
-            comic.ShowComicInformation();
-        }
-    }
-
-
-
-    private static bool IsValid(string email)
-    { 
-        string regex = @"^[^@\s]+@[^@\s]+\.(com|net|org|gov)$";
-        return Regex.IsMatch(email, regex, RegexOptions.IgnoreCase);
-    }
-
-
-    public void userRegister()
-    {
-
-        try
-        {
-            Console.WriteLine("___NUEVO USUARIO___");
-            Console.WriteLine("Nombre: ");
-            string name = Console.ReadLine();
-
-            //Validacion mail
-            string mail;
-            while(true)
-            {
-                Console.WriteLine("Correo: ");
-                mail = Console.ReadLine();
-                if (IsValid(mail))
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Error: El correo no es válido.");
-                }
-            }
-
-
-            string password;
-            while(true)
-            {
-                Console.WriteLine("Contraseña (Debe contener mínimo un número, una mayúscula y mínimo 8 carácteres): ");
-                password = Console.ReadLine();
-
-                var hasNumber = new Regex(@"[0-9]+");
-                var hasUpperChar = new Regex(@"[A-Z]+");
-                var hasMinimum8Chars = new Regex(@".{8,}");
-
-                var isValidated = hasNumber.IsMatch(password) && hasUpperChar.IsMatch(password) && hasMinimum8Chars.IsMatch(password);
-                
-                if (isValidated)
-                {
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Error: La contraseña no és válida.");
-                }
-            }
-
-            Console.WriteLine("Teléfono: ");
-            string telephone = Console.ReadLine();
-
-            User user = new User(name, mail, password, telephone);
-            Console.WriteLine("Usuario registrado correctamente");
-            user.ShowUserInformation();
-        } 
-        catch (InvalidUserException ex) 
-        {
-            var messageError = "InvalidUserException:" + ex.Message;
-            Console.WriteLine(messageError);
-        }
-        catch(Exception ex)
-        {
-            var messageError = "ExceptionError:" + ex.Message;
-        }
-        
-        
-        
-    }
-
-
-
-
-}   
+}
---
+++ Services/ComicService.cs
@@ -0,0 +1,198 @@
+namespace Services;
+using Models;
+
+class ComicService
+{
+    private static List<Comic> comics = new List<Comic>();
+
+
+    public ComicService()
+    {
+        comics = new List<Comic>();
+    }
+
+
+    public static void addComic() 
+    {
+        try
+        {
+            //Control de géneros antes de preguntar. Si no hay géneros, no se pueden añadir cómics
+            if (GenreService.genres.Count == 0)
+            {
+                Console.WriteLine("No hay géneros disponibles. Añade algunos géneros antes de seleccionar.");
+                return;
+            }
+
+
+            Console.WriteLine("___NUEVO CÓMIC___");
+            Console.WriteLine("Nombre: ");
+            string name = Console.ReadLine();
+
+
+            Console.WriteLine("Autor: ");
+            string author
+            = Console.ReadLine();
+
+
+            Console.WriteLine("Editorial: ");
+            string publisher = Console.ReadLine();
+
+
+            //Validación año
+            int yearPublished;
+            int currentYear = DateTime.Now.Year;
+            while (true)
+            {
+                Console.WriteLine("Año de Publicación: ");
+                if (int.TryParse(Console.ReadLine(), out yearPublished) && yearPublished >= 1896 && yearPublished <= currentYear)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine($"Error: El año debe ser entre 1896 y {currentYear}.");
+                }
+            }
+
+
+            //Validación precio
+            decimal price;
+            while (true)
+            {
+                Console.WriteLine("Precio: ");
+                if (decimal.TryParse(Console.ReadLine(), out price) && price > 0)
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error: El precio debe ser un número positivo.");
+                }
+            }
+
+
+            Console.WriteLine("Leído? (si/no): ");
+            string answer = Console.ReadLine();
+            bool isRead = answer.ToLower() == "yes" ? true : false;
+
+
+            Console.WriteLine("Es para adultos? (si/no): ");
+            answer = Console.ReadLine();
+            bool isForAdults = answer.ToLower() == "yes" ? true : false;
+
+
+            List<Genre> selectedGenres = new List<Genre>();
+            while(true)
+            {
+                Console.WriteLine("Géneros disponibles: ");
+                for (int i = 0; i < GenreService.genres.Count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {GenreService.genres[i].Name}");
+                }
+
+                Console.WriteLine("Introduce los números de los géneros separados por comas (ejemplo: 1,3,5):");
+                answer = Console.ReadLine();
+
+
+                if (string.IsNullOrWhiteSpace(answer))
+                {
+                    Console.WriteLine("Error: No has seleccionado ninguna categoría. Intentelo de nuevo.");
+                    continue;
+                }
+
+                string[] genreIndexArray = answer.Split(',');
+                selectedGenres.Clear();
+
+                bool isValid = true;
+
+                foreach (string index in genreIndexArray)
+                {
+                    if (int.TryParse(index.Trim(), out int genreIndex) && genreIndex > 0 && genreIndex <= GenreService.genres.Count)
+                    {
+                        selectedGenres.Add(GenreService.genres[genreIndex - 1]);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Error: La opción '{index}' marcada no es correcta. Intentelo de nuevo.");
+                        isValid = false;
+                        break;
+                    }
+                }
+
+                if (isValid && selectedGenres.Count > 0)
+                {
+                    break;
+                }
+            }
+
+
+            Console.WriteLine("Tipos de cómics:");
+            foreach (var comicType in Enum.GetValues(typeof(Comic.ComicType)))
+            {
+                Console.WriteLine($"{(int)comicType}. {comicType}");
+            }
+
+            Comic.ComicType selectedType;
+            while (true)
+            {
+                Console.WriteLine("Selecciona el tipo de cómic (número):");
+                if (int.TryParse(Console.ReadLine(), out int comicTypeSelection) && comicTypeSelection > 0 && comicTypeSelection <= Enum.GetValues(typeof(Comic.ComicType)).Length)
+                {
+                    selectedType = (Comic.ComicType)comicTypeSelection; // Casting del integer al enum
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error: Tipo de cómic no válido.");
+                }
+            }
+            
+            Comic comic= new Comic(name, author, publisher, yearPublished, price, isRead, isForAdults, selectedGenres, selectedType);
+            comic.Genres = selectedGenres;
+            comic.ShowComicInformation();
+
+            foreach (var genre in selectedGenres)
+            {
+                genre.Comics.Add(comic);
+            }
+
+            if (comics == null)
+            {
+                Console.WriteLine("La lista de cómics no está inicializada.");
+                return;
+            }
+
+            comics.Add(comic);
+                
+        }
+        catch (InvalidComicException ex) 
+        {
+            var messageError = "InvalidComicException:" + ex.Message;
+            Console.WriteLine(messageError);
+        }
+        catch(Exception ex)
+        {
+            var messageError = "ExceptionError:" + ex.Message;
+        }
+    }      
+
+
+
+
+    public static void showAllComics()
+    {
+        Console.WriteLine("\nListado de Cómics:");
+        foreach (var comic in comics)
+        {
+            if (comic != null)
+            {
+                comic.ShowComicInformation();
+            }
+            else
+            {
+                Console.WriteLine("Cósmico nulo encontrado en la lista.");
+            }
+        }
+    }
+
+}
